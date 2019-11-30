4955f75bff37199e78f11842c41a96617fec27d4
Altaf <altaf@groupbima.com>
2019-11-30 12:25:10 +0530

    initial commit

---
+++ src/main/java/altafjava/testing/App.java
@@ -0,0 +1,13 @@
+package altafjava.testing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
